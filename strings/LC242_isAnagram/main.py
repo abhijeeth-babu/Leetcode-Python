b9aae1c51a0fb622d6c8c0668cd97b242aa9763f
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-21 00:11:16 +0530

    isAnagram solved

--- strings/LC242_isAnagram/main.py
+++ strings/LC242_isAnagram/main.py
@@ -1,2 +1,16 @@
 class Solution:
     def isAnagram(self, s, t):
+        char_map = {}
+
+        for i, ch in enumerate(s):
+            char_map[ch] = char_map.get(ch, 0) + 1
+        
+        for i, ch in enumerate(t):
+            char_map[ch] = char_map.get(ch, 0) - 1
+            if char_map[ch] == 0:
+                del char_map[ch]
+        
+        return not char_map
+            
+        
+              
