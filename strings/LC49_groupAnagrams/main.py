0c830854277d10a17fb883b5fbba15bee4e85d20
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-21 00:53:24 +0530

    groupAnagrams completed

--- strings/LC49_groupAnagrams/main.py
+++ strings/LC49_groupAnagrams/main.py
@@ -1,2 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs):
+        word_map = {}
+        for word in strs:
+            sorted_word = ''.join(sorted(word))
+            if sorted_word not in word_map:
+                word_map[sorted_word] = []
+            word_map[sorted_word].append(word)
+        
+        return list(word_map.values())
+    
+obj = Solution()
+print(obj.groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']))
