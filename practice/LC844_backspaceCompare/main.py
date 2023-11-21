d6be43c20fab203dc71a2ab29b37b34291632d78
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-21 13:32:32 +0530

    lc 844 backspace compare completed

--- practice/LC844_backspaceCompare/main.py
+++ practice/LC844_backspaceCompare/main.py
@@ -1,2 +1,20 @@
 class Solution:
     def backspaceCompare(self, S, T):
+        st_s = []
+        st_t = []
+
+        for ch in S:
+            if ch == '#':
+                if st_s:
+                    st_s.pop()
+            else:
+                st_s.append(ch)
+        
+        for ch in T:
+            if ch == '#':
+                if st_t:
+                    st_t.pop()
+            else:
+                st_t.append(ch)
+        
+        return ''.join(st_s) == ''.join(st_t)
