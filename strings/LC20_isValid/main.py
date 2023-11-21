54118dd55f3f5ebe8f8d3ebedb46d414a98bc8b8
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-21 13:10:24 +0530

    lc20 is valid completed

--- strings/LC20_isValid/main.py
+++ strings/LC20_isValid/main.py
@@ -1,2 +1,19 @@
 class Solution:
     def isValid(self, s):
+        ch_map = {
+            ')':'(',
+            ']':'[',
+            '}':'{'
+        }
+        st = []
+        
+        for ch in s:
+            if ch in ['(', '[', '{']:
+                st.append(ch)
+            else:
+                if not st or st[-1] != ch_map[ch]:
+                    return False
+                st.pop()
+        
+        return not st
+
