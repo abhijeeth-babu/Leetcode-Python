c9c32fb2007894ea97312346c77ee507b103c39b
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-24 13:21:26 +0530

    completed lc70

--- dynamic_programming/LC70_climbStairs/main.py
+++ dynamic_programming/LC70_climbStairs/main.py
@@ -1,2 +1,11 @@
 class Solution:
+    dp = {}
     def climbStairs(self, n):
+        if n <= 3:
+            return n
+        elif n in self.dp:
+            return self.dp[n]
+        else:
+            self.dp[n] = self.climbStairs(n-1) + self.climbStairs(n-2)
+            return self.dp[n]
+
