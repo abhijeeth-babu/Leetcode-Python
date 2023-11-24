95ae353a750b96a2d5a47e5f15af12edcf8cad46
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-24 18:39:07 +0530

    lc 198 completed

--- dynamic_programming/LC198_rob/main.py
+++ dynamic_programming/LC198_rob/main.py
@@ -1,2 +1,12 @@
 class Solution:
     def rob(self, nums):
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[-1]
+        dp = [nums[0], max(nums[0], nums[1])]
+        for i in range(2, len(nums)):
+            dp.append(
+                max(dp[i-2]+nums[i], dp[i-1])
+            )
+        return dp[-1]
