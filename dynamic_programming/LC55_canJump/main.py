33e643bfb1ba0586434f3302517148b1a5f2592c
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-25 00:54:24 +0530

    lc 55 solved

--- dynamic_programming/LC55_canJump/main.py
+++ dynamic_programming/LC55_canJump/main.py
@@ -1,2 +1,10 @@
 class Solution:
     def canJump(self, nums):
+        max_distance = 0
+        for i in range(len(nums)):
+            if max_distance >= i:
+                max_distance = max(max_distance, i + nums[i])
+            else:
+                return False
+        return True
+            
