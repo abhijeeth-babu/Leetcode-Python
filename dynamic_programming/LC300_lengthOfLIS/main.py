f4098f017adf02ecb82b891d9f19f57a94f1b26f
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-25 16:03:15 +0530

    lc300 completed

--- dynamic_programming/LC300_lengthOfLIS/main.py
+++ dynamic_programming/LC300_lengthOfLIS/main.py
@@ -1,2 +1,15 @@
 class Solution:
     def lengthOfLIS(self, nums):
+        if not nums:
+            return 0
+        n = len(nums)
+        dp = [1] * n
+        msf = 1
+        for j in range(1, n):
+            for i in range(j):
+                if nums[j] > nums[i]:
+                    dp[j] = max(dp[j], dp[i]+1)
+
+            msf = max(msf, dp[j])
+        
+        return msf
