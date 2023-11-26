b5e054a769cd79cbc77385e009924d36615877b8
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-26 23:10:22 +0530

    lc11 updated

--- arrays/LC11_maxArea/main.py
+++ arrays/LC11_maxArea/main.py
@@ -1,2 +1,17 @@
 class Solution:
     def maxArea(self, height):
+        l, r = 0, len(height) - 1
+        max_vol = 0
+        max_ht = max(height)
+        while l < r:
+            if max_ht * (r - l) < max_vol: # optimization / not necessary
+                return max_vol
+            current_vol = min(height[l], height[r]) * (r - l)
+            max_vol = max(current_vol, max_vol)
+            if height[l] >= height[r]:
+                r -= 1
+            else:
+                l += 1
+
+        return max_vol
+            
