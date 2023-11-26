f2c28208c41c9d3e81457e071875ed50214d0299
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-26 19:36:27 +0530

    lc62 updated

--- dynamic_programming/LC62_uniquePaths/main.py
+++ dynamic_programming/LC62_uniquePaths/main.py
@@ -1,14 +1,7 @@
 class Solution:
     def uniquePaths(self, m, n):
-        if m == 1 and n == 1:
-            return 1
         
-        dp = [[0 for _ in range(n)] for _ in range(m)]
-        for i in range(1, m):
-            dp[i][0] = 1
-        
-        for i in range(1, n):
-            dp[0][i] = 1
+        dp = [[1 for _ in range(n)] for _ in range(m)]
 
         for i in range(1, m):
             for j in range(1, n):
