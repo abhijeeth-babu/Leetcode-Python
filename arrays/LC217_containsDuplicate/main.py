195baf088b739bda1a2ed3ed801d71d35dec585d
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-26 19:44:21 +0530

    lc 217 solved

--- arrays/LC217_containsDuplicate/main.py
+++ arrays/LC217_containsDuplicate/main.py
@@ -1,2 +1,11 @@
 class Solution:
     def containsDuplicate(self, nums):
+        num_set = set()
+
+        for num in nums:
+            if num in num_set:
+                return True
+            else:
+                num_set.add(num)
+        
+        return False
