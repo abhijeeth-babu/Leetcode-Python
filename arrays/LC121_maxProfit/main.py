e0126f22a9f547ff4144b05cb2553d09cf7b8432
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-27 00:21:56 +0530

    lc 121 solved

--- arrays/LC121_maxProfit/main.py
+++ arrays/LC121_maxProfit/main.py
@@ -1,2 +1,11 @@
 class Solution:
     def maxProfit(self, prices):
+        max_profit = 0
+        current_min = prices[0]
+
+        for price in prices:
+            current_min = min(current_min, price)
+            current_profit = price - current_min
+            max_profit = max(max_profit, current_profit)
+
+        return max_profit
