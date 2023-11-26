e49349fa3e000cb1e072a775e3f7690913fa7af9
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-26 15:01:27 +0530

    lc 322 solved

--- dynamic_programming/LC322_coinChange/main.py
+++ dynamic_programming/LC322_coinChange/main.py
@@ -1,2 +1,16 @@
 class Solution:
     def coinChange(self, coins, amount):
+        dp = [amount+1] * (amount+1)
+        dp[0] = 0
+
+        for amt in range(1, amount+1):
+            for coin in coins:
+                change = amt - coin
+                if change >= 0:
+                    dp[amt] = min(
+                        1 + dp[change],
+                        dp[amt]
+                    )
+        
+        return dp[-1] if dp[-1] != amount + 1 else -1
+
