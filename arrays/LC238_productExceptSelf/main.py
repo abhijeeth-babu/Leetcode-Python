483245303f372d08d540e64faf9d85e6b02ecec3
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-26 20:58:01 +0530

    lc 238 solved

--- arrays/LC238_productExceptSelf/main.py
+++ arrays/LC238_productExceptSelf/main.py
@@ -1,2 +1,17 @@
 class Solution:
     def productExceptSelf(self, nums):
+        n = len(nums)
+        res = [1 for _ in range(n)]
+
+        psf = 1
+        for i in range(1, n):
+            psf = nums[i-1] * psf
+            res[i] = psf
+
+        psf = 1
+
+        for i in range(n-2, -1,-1):
+            psf = nums[i+1] * psf
+            res[i] *= psf
+        
+        return res
