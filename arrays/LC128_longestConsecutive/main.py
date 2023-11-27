3e542f77ddb4dd9a6c8eb2d29dc4a676c59d1a1d
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-28 00:28:39 +0530

    lc 128 solved

--- arrays/LC128_longestConsecutive/main.py
+++ arrays/LC128_longestConsecutive/main.py
@@ -1,2 +1,13 @@
 class Solution:
     def longestConsecutive(self, nums):
+        num_set = set(nums)
+        lcs = 0
+        for num in nums:
+            length = 0
+            if num - 1 not in num_set:
+                while num + length in num_set:
+                    length += 1
+            lcs = max(lcs, length)
+
+        return lcs   
+            
