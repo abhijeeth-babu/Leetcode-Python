6ad5950b71b9fa95b4295465f835f27c9468fb12
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-28 22:07:48 +0530

    lc 152 solved

--- arrays/LC152_maxProduct/main.py
+++ arrays/LC152_maxProduct/main.py
@@ -1,2 +1,22 @@
 class Solution:
     def maxProduct(self, nums):
+        curr_max = nums[0]
+        curr_min = nums[0]
+        msf = max(curr_max, curr_min)
+        for i in range(1, len(nums)):
+            a = nums[i]
+            b = curr_max * nums[i]
+            c = curr_min * nums[i]
+            curr_max = max(
+                a,
+                b,
+                c
+            )
+            curr_min = min(
+                a,
+                b,
+                c
+            )
+            msf = max(msf, curr_max)
+        
+        return msf
