ab7987418e5b3320edc186fbd1fc6726026a0796
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-28 22:10:28 +0530

    lc153 solved

--- arrays/LC153_findMin/main.py
+++ arrays/LC153_findMin/main.py
@@ -1,2 +1,16 @@
 class Solution:
     def findMin(self, nums):
+        l, r = 0, len(nums) - 1
+        if nums[l] < nums[r]:
+            return nums[l]
+        
+        while l < r:
+            m = (l + r) // 2
+            if nums[m-1] > nums[m] and nums[m+1] > nums[m]:
+                return nums[m]
+            elif nums[m] > nums[r]:
+                l = m + 1
+            else:
+                r = m - 1
+        
+        return nums[l]
