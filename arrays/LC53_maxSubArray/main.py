ae4b57fcfc249a053d80a50f35bf7e128c058d97
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-28 13:18:51 +0530

    lc 53 solved

--- arrays/LC53_maxSubArray/main.py
+++ arrays/LC53_maxSubArray/main.py
@@ -1,2 +1,10 @@
 class Solution:
     def maxSubArray(self, nums):
+        curr_sum = nums[0]
+        msf = nums[0]
+
+        for i in range(1, len(nums)):
+            curr_sum = max(nums[i], curr_sum+nums[i])
+            msf = max(curr_sum, msf)
+        
+        return msf
