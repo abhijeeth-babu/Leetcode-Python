2538f4b21b1c13747b499d6f32c1ee4e88395629
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-11-29 16:03:28 +0530

    lc 252 solved

--- intervals/LC252_canAttendMeetings/main.py
+++ intervals/LC252_canAttendMeetings/main.py
@@ -8,3 +8,11 @@
 
 class Solution:
     def canAttendMeetings(self, intervals):
+        intervals = sorted(intervals, key=lambda x: [x[0], x[1]])
+        for i in range(1, len(intervals)):
+            prev_meeting_end = intervals[i-1][-1]
+            curr_meeting_start = intervals[i][0]
+            if prev_meeting_end > curr_meeting_start:
+                return False
+        
+        return True
