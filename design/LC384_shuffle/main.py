e333d5137a9a54be8b2ba8cb01afca78fb7cdf0c
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-04 00:08:36 +0530

    lc 384 completed

--- design/LC384_shuffle/main.py
+++ design/LC384_shuffle/main.py
@@ -4,7 +4,18 @@ import random
 class Solution:
 
     def __init__(self, nums):
+        self.arr = nums
+        self.org = nums.copy()
+
 
     def reset(self):
-
+        self.arr = self.org.copy()
+        return self.arr
+    
+    
     def shuffle(self):
+        for idx in range(len(self.arr)):
+            ridx = random.randrange(idx, len(self.arr))
+            self.arr[idx], self.arr[ridx] = self.arr[ridx], self.arr[idx]
+        
+        return self.arr
