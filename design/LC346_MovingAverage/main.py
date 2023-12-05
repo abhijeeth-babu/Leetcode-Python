29a9a149ed6522c7ecdf6f6d1e3e0a88db370526
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-05 18:04:15 +0530

    lc 346 solved

--- design/LC346_MovingAverage/main.py
+++ design/LC346_MovingAverage/main.py
@@ -7,9 +7,21 @@
 # m.next(3) = (3 + 10 + 1) --> 4.66667
 # m.next(5) = (5 + 3 + 10) --> 6
 
-
+# use q for O(1) in and out ops
+from collections import deque
 class MovingAverage:
 
     def __init__(self, size):
+        self.window = deque()
+        self.size = size
+        self.sum = 0
+
 
     def next(self, val):
+        if len(self.window) == self.size:
+            self.sum -= self.window.popleft()
+        self.window.append(val)
+        self.sum += val
+        return self.sum / len(self.window)
+    
+
