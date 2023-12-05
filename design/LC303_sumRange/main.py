55284071f53e38db1af3d96576ff38108732e010
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-06 00:42:31 +0530

    lc 303 solved

--- design/LC303_sumRange/main.py
+++ design/LC303_sumRange/main.py
@@ -1,5 +1,14 @@
 class NumArray:
 
     def __init__(self, nums):
+        self.cumsum = nums
+        for i in range(1, len(nums)):
+            self.cumsum[i] += self.cumsum[i-1]
+        
 
     def sumRange(self, i, j):
+        if i > 0:
+            return self.cumsum[j] - self.cumsum[i-1]
+        else:
+            return self.cumsum[j]
+        
