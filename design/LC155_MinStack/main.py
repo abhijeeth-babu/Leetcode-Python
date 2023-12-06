e0152c8c2ac0680bb0811da1286dfb25b9f97ffd
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-06 14:13:48 +0530

    lc 155 completed

--- design/LC155_MinStack/main.py
+++ design/LC155_MinStack/main.py
@@ -1,11 +1,41 @@
+class Node:
+    
+    def __init__(self, val) -> None:
+       self.val = val
+       self.next = None
+       self.min = None
+
+
 class MinStack:
 
     def __init__(self):
+        self.last = None
 
     def push(self, x):
+        if not self.last:
+            self.last = Node(x)
+            self.last.min = x
+            return
+        temp = Node(x)
+        temp.min = min(self.last.min, x)
+        temp.next = self.last
+        self.last =  temp
+
 
     def pop(self):
+        if not self.last:
+            return
+        popped = self.last.val
+        self.last = self.last.next
+        return popped
 
     def top(self):
+        if not self.last:
+            return
+        return self.last.val
+    
 
     def getMin(self):
+        if not self.last:
+            return
+        return self.last.min
