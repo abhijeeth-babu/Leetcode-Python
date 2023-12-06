b5752c571544eddce037b9ea6b978a1e892c6893
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-06 14:25:09 +0530

    alternative sol for lc 155

--- design/LC155_MinStack/main.py
+++ design/LC155_MinStack/main.py
@@ -39,3 +39,31 @@ class MinStack:
         if not self.last:
             return
         return self.last.min
+
+# alternatice solution
+
+# class MinStack:
+
+#     def __init__(self):
+#         self.st = []
+#         self.min_st = []
+        
+
+#     def push(self, val: int) -> None:
+#         self.st.append(val)
+#         if not self.min_st:
+#             self.min_st.append(val)
+#         else:
+#             self.min_st.append(min(self.min_st[-1], self.st[-1]))
+        
+
+#     def pop(self) -> None:
+#         self.min_st.pop()
+#         return self.st.pop()
+
+#     def top(self) -> int:
+#         return self.st[-1]
+        
+
+#     def getMin(self) -> int:
+#         return self.min_st[-1]
