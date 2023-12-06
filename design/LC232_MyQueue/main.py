341434edb2bb4169ad65ac7d7207e7ffed8c053d
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-06 18:41:54 +0530

    lc 232 completed

--- design/LC232_MyQueue/main.py
+++ design/LC232_MyQueue/main.py
@@ -4,23 +4,35 @@ class MyQueue:
         """
         Initialize your data structure here.
         """
+        self.st = []
+        self.helper = []
 
     def push(self, x):
         """
         Push element x to the back of queue.
         """
+        self.st.append(x)
 
     def pop(self):
         """
         Removes the element from in front of queue and returns that element.
         """
+        while self.st:
+            self.helper.append(self.st.pop())
+        popped = self.helper.pop()
+        while self.helper:
+            self.st.append(self.helper.pop())
+        return popped
+    
 
     def peek(self):
         """
         Get the front element.
         """
+        return self.st[0]
 
     def empty(self):
         """
         Returns whether the queue is empty.
         """
+        return not self.st
