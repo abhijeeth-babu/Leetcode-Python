1660bb16423df979e2b11699ac7be6582be5ffe5
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-07 20:00:44 +0530

    lc 841 completed

--- practice/LC876_middleNode/main.py
+++ practice/LC876_middleNode/main.py
@@ -1,2 +1,8 @@
 class Solution:
     def middleNode(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        return slow
