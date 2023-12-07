d5259b1f73cb47cf1dbee4aec0129db01e7801d6
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-07 15:31:07 +0530

    lc 206 solved

--- linked_lists/LC206_reverseList/main.py
+++ linked_lists/LC206_reverseList/main.py
@@ -6,3 +6,9 @@
 
 class Solution:
     def reverseList(self, head):
+        if not head or not head.next:
+            return head
+        new_head = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return new_head
