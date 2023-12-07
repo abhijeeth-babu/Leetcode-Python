ee1c0eb2273becd5188e3e9db942e1d5d746f85d
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-07 19:59:08 +0530

    lc 141 completed

--- linked_lists/LC141_hasCycle/main.py
+++ linked_lists/LC141_hasCycle/main.py
@@ -6,3 +6,13 @@
 
 class Solution:
     def hasCycle(self, head):
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+            
+        return False
