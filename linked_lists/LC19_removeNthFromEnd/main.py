7631af8750af793308a9aaa1c483f51345f9793a
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-08 00:02:53 +0530

    lc19 completed

--- linked_lists/LC19_removeNthFromEnd/main.py
+++ linked_lists/LC19_removeNthFromEnd/main.py
@@ -6,3 +6,18 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head, n):
+        temp = head
+        sz = 0
+        while temp:
+            sz += 1
+            temp = temp.next
+        
+        dummy = ListNode(0)
+        dummy.next = head
+        temp = dummy
+        k = sz + 1 - n
+        for _ in range(k-1):
+            temp = temp.next
+        temp.next = temp.next.next
+        
+        return dummy.next
