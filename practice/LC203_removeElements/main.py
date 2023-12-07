177b8d6b12210161a73182cd5a82868484243b5a
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-08 00:36:48 +0530

    lc 203 completed

--- practice/LC203_removeElements/main.py
+++ practice/LC203_removeElements/main.py
@@ -6,3 +6,13 @@ class ListNode:
 
 class Solution:
     def removeElements(self, head, val):
+        dummy = ListNode(0)
+        dummy.next = head
+        temp = dummy
+        while temp:
+            if temp.next and temp.next.val == val:
+                temp.next = temp.next.next
+            else:
+                temp = temp.next
+        
+        return dummy.next
