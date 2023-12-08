cd62075131ce19b9ecdbf6987c91b8ff54a098c3
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-08 10:10:55 +0530

    lc 21

--- linked_lists/LC21_mergeTwoLists/main.py
+++ linked_lists/LC21_mergeTwoLists/main.py
@@ -6,3 +6,17 @@ class ListNode:
 
 class Solution:
     def mergeTwoLists(self, l1, l2):
+        dummy = ListNode(0)
+        temp = dummy
+        while l1 and l2:
+            if l1.val < l2.val:
+                temp.next = l1
+                l1 = l1.next
+            else:
+                temp.next = l2
+                l2 = l2.next
+            temp = temp.next
+        
+        temp.next = l1 or l2
+        
+        return dummy.next
