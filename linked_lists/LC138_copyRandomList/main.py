fff6ed0df85076730445ec075feb13d5be60e9a4
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-10 23:53:28 +0530

    lc 138 solved

--- linked_lists/LC138_copyRandomList/main.py
+++ linked_lists/LC138_copyRandomList/main.py
@@ -7,3 +7,19 @@ class Node:
 
 class Solution:
     def copyRandomList(self, head):
+        temp = head
+        # dict that tracks orginal and copy nodes
+        d = {}
+
+        # add org as keys and create copy as values
+        while temp:
+            d[temp] = Node(temp.val, None, None)
+            temp = temp.next
+        
+        # copy.next = value of org.next in dict
+        # copy.random = value of org.random in dict
+        for org in d.keys():
+            d[org].next = d.get(org.next, None)
+            d[org].random = d.get(org.random, None)
+        
+        return d.get(head, None)
