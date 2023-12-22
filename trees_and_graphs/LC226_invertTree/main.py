71acb93cf64f84f9855d796a52c8f0f0d8a5cc10
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-22 08:24:40 +0530

    lc 226 solved

--- trees_and_graphs/LC226_invertTree/main.py
+++ trees_and_graphs/LC226_invertTree/main.py
@@ -1,2 +1,12 @@
 class Solution:
     def invertTree(self, root):
+        if not root:
+            return root
+
+        right = self.invertTree(root.left)
+
+        left = self.invertTree(root.right)
+
+        root.right, root.left = right, left
+
+        return root
