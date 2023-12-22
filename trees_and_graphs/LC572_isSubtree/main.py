9c4e9d9d0ec9033185de0ce5ff8781923f7d6785
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-22 17:13:58 +0530

    lc 572 solved

--- trees_and_graphs/LC572_isSubtree/main.py
+++ trees_and_graphs/LC572_isSubtree/main.py
@@ -1,2 +1,25 @@
 class Solution():
     def isSubtree(self, s, t):
+        if not s:
+            return False
+        
+        if not t:
+            return False
+        
+        if self.isSameTree(s, t):
+            return True
+        
+        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+    
+    
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return True
+        
+        if not p or not q:
+            return False
+        
+        if p.val != q.val:
+            return False
+        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
