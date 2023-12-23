a6aa67910db97de3f484c7641f27b9499630a88c
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-23 15:59:20 +0530

    lc 98 solved

--- trees_and_graphs/LC98_isValidBST/main.py
+++ trees_and_graphs/LC98_isValidBST/main.py
@@ -1,2 +1,12 @@
 class Solution:
     def isValidBST(self, root):
+        return self.helper(root, float('-inf'), float('inf'))
+    
+    def helper(self, node, min_val, max_val):
+        if not node:
+            return True
+        
+        if not (min_val < node.val < max_val):
+            return False
+        
+        return self.helper(node.left, min_val, node.val) and self.helper(node.right, node.val, max_val)
