508b9f0f514595840c26a2107ffbd0a719bc60b8
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-23 19:59:17 +0530

    lc 230 solved

--- trees_and_graphs/LC230_kthSmallest/main.py
+++ trees_and_graphs/LC230_kthSmallest/main.py
@@ -1,2 +1,15 @@
 class Solution:
     def kthSmallest(self, root, k):
+        res = self.inorder(root)
+
+        return res[k-1]
+    
+    def inorder(self, node):
+        if not node:
+            return []
+        
+        left = self.inorder(node.left)
+        right = self.inorder(node.right)
+
+        return left + [node.val] + right
+    
