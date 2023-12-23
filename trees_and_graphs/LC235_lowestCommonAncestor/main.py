8a443538a2286510e5134366f9a6ca9013627106
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-23 19:39:04 +0530

    lc 235 colved

--- trees_and_graphs/LC235_lowestCommonAncestor/main.py
+++ trees_and_graphs/LC235_lowestCommonAncestor/main.py
@@ -1,2 +1,10 @@
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
+        curr = root
+        while curr:
+            if curr.val > p.val and curr.val > q.val:
+                curr = curr.left
+            elif curr.val < p.val and curr.val < q.val:
+                curr = curr.right
+            else:
+                return curr
