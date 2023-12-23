04ecc6ca20e23854dfbcf08edc86d6405dce7705
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-23 19:44:20 +0530

    lc235 recursion

--- trees_and_graphs/LC235_lowestCommonAncestor/main.py
+++ trees_and_graphs/LC235_lowestCommonAncestor/main.py
@@ -8,3 +8,12 @@ class Solution:
                 curr = curr.right
             else:
                 return curr
+            
+    # recursive      
+    # def lowestCommonAncestor(self, root, p, q):
+    #     if root.val > p.val and root.val > q.val:
+    #         return self.lowestCommonAncestor(root.left, p, q)
+    #     elif root.val < p.val and root.val < q.val:
+    #         return self.lowestCommonAncestor(root.right, p, q)
+    #     else:
+    #         return root
