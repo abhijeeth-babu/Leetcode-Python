095cd6858cd1d0620e6be3c3d89806ba4015ced3
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-24 10:31:48 +0530

    lc 331 solved

--- trees_and_graphs/LC331_isValidSerialization/main.py
+++ trees_and_graphs/LC331_isValidSerialization/main.py
@@ -1,2 +1,15 @@
 class Solution:
     def isValidSerialization(self, preorder):
+        nodes = preorder.split(",")
+        spots = 1
+
+        for node in nodes:
+            spots -= 1
+
+            if spots < 0:
+                return False
+
+            if node != "#":
+                spots += 2
+
+        return spots == 0
