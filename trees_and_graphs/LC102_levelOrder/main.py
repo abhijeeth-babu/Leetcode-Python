e0762ffd3a65d8f43b0ddeab1a98a5006aa8d14d
Abhijeeth Babu <abhijeethbabu@gmail.com>
2023-12-24 10:36:20 +0530

    lc 102 solved

--- trees_and_graphs/LC102_levelOrder/main.py
+++ trees_and_graphs/LC102_levelOrder/main.py
@@ -1,2 +1,24 @@
+from collections import deque
+
+
 class Solution:
     def levelOrder(self, root):
+        if not root:
+            return []
+        
+        q = deque([root])
+        res = []
+        while q:
+            curr_level = []
+            for _ in range(len(q)):
+                node = q.popleft()
+                curr_level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            res.append(curr_level)
+        
+        return res
